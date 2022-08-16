e1b2f216ffd917c8799b0f964ae2537648c6109d
sissoko86 <sissoko_o@yahoo.fr>
2022-08-16 15:47:14 +0200

    axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Question } from "./questions";
-import { JOEL_QUESTIONS } from "./data";
+//import { JOEL_QUESTIONS } from "./data";
 import "./styles/scss/common.scss";
+import axios from "axios";
 
 function App() {
-  const [questions] = useState<Question[]>(JOEL_QUESTIONS);
+  const [questions, setQuestions] = useState<Question[]>();
   const [clicked, setClicked] = useState<object>({});
   type objectKey = keyof typeof clicked;
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  const getQuestions: () => Promise<void> = async () => {
+    const { data } = await axios.get<Question[]>(
+      "http://localhost:8086/api/questions",
+
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+    // console.log("data", data);
+    setQuestions(data);
+    //return data;
+  };
 
   const getClass = (index: number) => {
     return `shadow  ${
